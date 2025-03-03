2343d368261761c6b2a6abce3f9851489a17df05
Kevin Deng <2363433@edu.vaniercollege.qc.ca>
2025-03-03 12:50:17 -0500

    Sunny's changes

--- Deliverable 3 Part 1/PhysiPlay/src/main/java/com/example/physiplay/MainApp.java
+++ Deliverable 3 Part 1/PhysiPlay/src/main/java/com/example/physiplay/MainApp.java
@@ -38,7 +38,7 @@ public final class MainApp extends Application {
         //For Main Menu
 
         Scene scene = new Scene(new Pane(), 1920, 1080);
-
+        Font.loadFont(Objects.requireNonNull(getClass().getResource("/fonts/madimione-regular.otf").toExternalForm()), 50);
         loadScenes(stage);
 
         ScreenController.getInstance().setMainScene(scene);
