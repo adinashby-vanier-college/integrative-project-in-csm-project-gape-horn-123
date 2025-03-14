4ff2f554447b31138eb09891f9402fa61884a871
KyunSC <exsunnychen2006@gmail.com>
2025-03-14 15:04:26 -0400

    Create Preset FXML

--- Deliverable 3 Part 1/PhysiPlay/src/main/java/com/example/physiplay/controllers/PhysiplayController.java
+++ Deliverable 3 Part 1/PhysiPlay/src/main/java/com/example/physiplay/controllers/PhysiplayController.java
@@ -1,12 +1,14 @@
 package com.example.physiplay.controllers;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PhysiplayController {
 
@@ -17,7 +19,13 @@ public class PhysiplayController {
 
     public void initialize() {
         createPresetButton.setOnAction(event -> {
-            Parent root = new Pane();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/createPreset.fxml"));
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             Stage presetWindow = new Stage();
             Scene scene = new Scene(root, 500, 900);
             presetWindow.setScene(scene);
