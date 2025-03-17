afd5be025540f6fbd7b055b3a9b7229771c50d5b
KyunSC <exsunnychen2006@gmail.com>
2025-03-17 00:41:00 -0400

    Tab content using FXML

--- Deliverable 3 Part 1/PhysiPlay/src/main/java/com/example/physiplay/components/DragShapeHandler.java
+++ Deliverable 3 Part 1/PhysiPlay/src/main/java/com/example/physiplay/components/DragShapeHandler.java
@@ -1,12 +1,17 @@
 package com.example.physiplay.components;
 
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
+
+import java.io.IOException;
 
 public class DragShapeHandler implements EventHandler<MouseEvent> {
 
@@ -27,11 +32,36 @@ public class DragShapeHandler implements EventHandler<MouseEvent> {
         }
         else if (event.getEventType() == MouseEvent.MOUSE_RELEASED){
             gc.fillRect(event.getSceneX() - 360, event.getSceneY() - 35, 10, 10);
-            tabPane.getTabs().add(new Tab("GameObject 1"));
+            Tab tab = new Tab("GameObject 1");
+            tabPane.getTabs().add(tab);
+
+            try {
+                tab.setContent(makeTabContent());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     public void setTabPane(TabPane tabPane){
         this.tabPane = tabPane;
+    }
+
+    public ScrollPane makeTabContent() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/gameObjectTab.fxml"));
+        /*VBox vBox = new VBox();
+        HBox hBox = new HBox();
+        Rectangle rectangle = new Rectangle(10,10);
+        Label label = new Label("GameObject 1");
+        hBox.getChildren().addAll(rectangle, label);
+        Accordion accordion = new Accordion();
+        TitledPane transformPane = new TitledPane();
+        AnchorPane anchorPane = new AnchorPane();
+
+        TitledPane rigidBody = new TitledPane();
+        TitledPane audio = new TitledPane();
+        accordion.getPanes().addAll(transformPane, rigidBody, audio);*/
+
+        return loader.load();
     }
 }
