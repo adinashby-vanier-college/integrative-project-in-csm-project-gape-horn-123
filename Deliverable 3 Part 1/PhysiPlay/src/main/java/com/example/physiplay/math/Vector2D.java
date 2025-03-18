cc843f962fccf27b9ef7a4abc0b76f7037bc1b6e
craponyourlap <143549907+craponyourlap@users.noreply.github.com>
2025-03-18 01:01:40 -0400

    vector2d multiplication

--- Deliverable 3 Part 1/PhysiPlay/src/main/java/com/example/physiplay/math/Vector2D.java
+++ Deliverable 3 Part 1/PhysiPlay/src/main/java/com/example/physiplay/math/Vector2D.java
@@ -1,5 +1,24 @@
 package com.example.physiplay.math;
 
 public class Vector2D {
-
+	
+	private double x, y;
+	
+	public Vector2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}	
+	
+	public Vector2D add(Vector2D other) {
+		return new Vector2D(this.x + other.x, this.y + other.y);
+	}
+	
+	public Vector2D subtract(Vector2D other) {
+		return new Vector2D(this.x - other.x, this.y - other.y);
+	}
+	
+	public Vector2D multiply(double scalar) {
+		return new Vector2D(this.x * scalar, this.y * scalar);
+	}
+	
 }
