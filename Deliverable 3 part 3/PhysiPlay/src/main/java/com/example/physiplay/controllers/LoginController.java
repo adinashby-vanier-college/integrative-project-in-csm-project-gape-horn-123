cc21ade18cc187b6440db5cbc2c2c240c114e2d6
KyunSC <exsunnychen2006@gmail.com>
2025-04-15 15:46:29 -0400

    Language stuff

--- Deliverable 3 part 3/PhysiPlay/src/main/java/com/example/physiplay/controllers/LoginController.java
+++ Deliverable 3 part 3/PhysiPlay/src/main/java/com/example/physiplay/controllers/LoginController.java
@@ -4,7 +4,12 @@ import com.example.physiplay.singletons.SettingsSingleton;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class LoginController {
 
@@ -17,6 +22,16 @@ public class LoginController {
     Button registerButton;
     @FXML
     Button languageButton;
+    @FXML
+    BorderPane rootPane;
+    @FXML
+    Label loginLabel;
+    @FXML
+    Label usernameLabel;
+    @FXML
+    Label passwordLabel;
+
+
 
     public LoginController(Stage stage, Scene scene){
         this.stage = stage;
@@ -57,13 +72,27 @@ public class LoginController {
         });
         languageButton.setOnAction(event -> {
             SettingsSingleton.getInstance().switchLanguage();
+            Locale locale = new Locale(SettingsSingleton.getInstance().language);
+            ResourceBundle bundle = ResourceBundle.getBundle("languages.messages", locale);
+            loginLabel.setText(bundle.getString("label.loginPage"));
+            usernameLabel.setText(bundle.getString("label.username"));
+            passwordLabel.setText(bundle.getString("label.password"));
+            loginButton.setText(bundle.getString("button.login"));
+            registerButton.setText(bundle.getString("button.register"));
+
+            /*SettingsSingleton.getInstance().switchLanguage();
             ScreenController.getInstance().clearMap();
             ScreenController.getInstance()
                     .addScreen("loginPage", ScreenController.getInstance().getRootPane(getClass().getResource("/fxml/loginPage.fxml"), new LoginController(stage, scene), SettingsSingleton.getInstance().language))
                     .addScreen("mainMenu", ScreenController.getInstance().getRootPane(getClass().getResource("/fxml/mainMenu.fxml"), new MainMenuController(stage, scene), SettingsSingleton.getInstance().language))
                     .addScreen("register", ScreenController.getInstance().getRootPane(getClass().getResource("/fxml/registerPage.fxml"), new RegisterPageController(stage, scene), SettingsSingleton.getInstance().language));
-            ScreenController.getInstance().activate("loginPage");
+            ScreenController.getInstance().activate("loginPage");*/
         });
+
+    }
+
+    public void loadPages(){
+        rootPane.getChildren().setAll(ScreenController.getInstance().getRootPane(getClass().getResource("/fxml/loginPage.fxml"), new LoginController(stage, scene), SettingsSingleton.getInstance().language));
 
     }
 
