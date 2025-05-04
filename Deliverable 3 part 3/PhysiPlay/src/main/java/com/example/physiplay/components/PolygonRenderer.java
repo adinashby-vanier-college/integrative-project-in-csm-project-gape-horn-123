751070a3638273092d2c74d63f8cdfdeaac9c6bf
Kevin Deng <dengkthatsme@gmail.com>
2025-05-04 00:56:55 -0400

    Real time polygon color change

--- Deliverable 3 part 3/PhysiPlay/src/main/java/com/example/physiplay/components/PolygonRenderer.java
+++ Deliverable 3 part 3/PhysiPlay/src/main/java/com/example/physiplay/components/PolygonRenderer.java
@@ -2,9 +2,7 @@ package com.example.physiplay.components;
 
 import com.example.physiplay.Vector2;
 import com.example.physiplay.singletons.SimulationManager;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.*;
 import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
 
@@ -12,11 +10,18 @@ import java.util.*;
 
 public class PolygonRenderer extends Renderer {
     public List<Vector2> positions = new ArrayList<>();
+
+    private ComponentPropertyBuilder polygonPropertyBuilder = new ComponentPropertyBuilder()
+            .addLabelProperty("colorLabel", "Color", new Label())
+            .addColorPickerProperty("color", "Choose color:", new ColorPicker());
+            /*.addTextFieldProperty("positions", "Enter positions:", new TextField())
+            .addLabelProperty("notsure", "Not sure? Go to Window -> Polygon Visualizer", new Label());*/
     @Override
     public void initializeShapeCollider() {
         PolygonShape shape = new PolygonShape();
         shape.set(getVerticesOfPolygon(), positions.size());
         parent.fixtureDef.shape = shape;
+        updateValues();
     }
     private Vec2[] getVerticesOfPolygon() {
         Vec2[] points = new Vec2[positions.size()];
@@ -25,6 +30,14 @@ public class PolygonRenderer extends Renderer {
                     (float) positions.get(i).y / SimulationManager.SCALE);
         }
         return points;
+    }
+
+    private void updateValues() {
+        ColorPicker picker = polygonPropertyBuilder.getColorPicker("color");
+        picker.valueProperty().setValue(color);
+        picker.valueProperty().addListener((obs, oldVal, newVal) -> {
+            color = newVal;
+        });
     }
 
     private double[] getVerticesIndividualCoordinates(int index) {
@@ -53,9 +66,10 @@ public class PolygonRenderer extends Renderer {
 
     @Override
     public void displayComponent() {
+        componentTab.getStyleClass().add(Objects.requireNonNull(getClass().getResource("/css/tabStylesheet.css")).toExternalForm());
         componentTab.setText("Polygon Renderer");
-        Label label = new Label("In Progress");
-        label.setStyle("-fx-font-size: 20px");
-        componentTab.setContent(label);
+        componentTab.setContent(polygonPropertyBuilder.getAllProperties());
     }
+
+
 }
